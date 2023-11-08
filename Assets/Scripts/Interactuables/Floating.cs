1d488b29358aba240d6ba623be96d77b745e4800
Master021 <julianabaroa38@gmail.com>
2023-11-08 12:00:45 -0300

    ActionRPG project

---
+++ Assets/Scripts/Interactuables/Floating.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floating : Interactables
+{
+
+    protected override void Awake() {
+        base.Awake();
+    }
+
+    private void Update() {
+        
+    }
+
+    public override void Interact() {
+
+    }
+}
